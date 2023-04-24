0617a122a6f689d57d3d54f24afb812a3041478a
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-24 07:04:52 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while (x <= 'z' != 'q', 'e');
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
