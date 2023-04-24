57bafdfaf3b3ec6d09c8ff6e323742195b61c5c4
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-24 07:30:46 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main : Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	while (x >= 0 && x <= 10);
+	{
+		printf("%d\n", x);
+		x++;
+	}
+	return (0);
+}
