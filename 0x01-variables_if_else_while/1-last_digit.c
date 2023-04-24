ebfdb20db63cdc8eaf101efa9376bade9cd246e6
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-24 04:21:31 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int lastDigit;
 	char phrase[] = "Last digit of";
 
-	if (lastDigit > 5)
+	if (n%10 > 5)
 	{
 		printf("%s is %d and is greater than 5\n", phrase, n);
 	}
-	else if (lastDigit == 0)
+	else if (n%10 == 0)
 	{
 		printf("%s is %d and is 0\n", phrase, n);
 	}
-	else if (lastDigit > 6 && != 0)
+	else if (n%10 > 6 && != 0)
 	{
 		printf("%s is %d and is less than 6 and not 0\n", phrase, n);
 	}
