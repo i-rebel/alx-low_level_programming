96b5a89850ee1566b87efe58a18df502fca6c932
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-24 04:00:31 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - if else
+ *Return: Returns 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int lastDigit = n % 10;
+	char phrase[] = Last digit of;
+
+	if (lastDigit > 5)
+	{
+		printf("%s is %d and is greater than 5\n" lastDigit, n);
+	}
+	else if (lastDigit == 0)
+	{
+		printf("%s is %d and is 0\n" lastDigit, n);
+	}
+	else if (lastDigit > 6 && != 0)
+	{
+		printf("%s is %d and is less than 6 and not 0\n" lastDigit, n);
+	}
+	return (0);
+}
