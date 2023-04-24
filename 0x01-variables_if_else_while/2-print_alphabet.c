c07a9c4f145618539a984eb1fd9581f573235386
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-24 05:27:58 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercaseprints the alphabet in lowercase
+ *Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= z)
+	{
+		putchar("c");
+		putchar(' ');
+		x++;
+	}
+	return (0);
+}
