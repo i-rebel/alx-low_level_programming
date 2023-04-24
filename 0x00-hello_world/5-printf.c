43e4fb8010d14b5446dccf2727c271b057ba0372
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-23 22:58:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Prints out with proper grammar, but the outcome is a piece of art
+ *Return: Returns 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
