5de8c9aa36d3b2091eec5a7c96c431cf02811a98
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-24 10:50:51 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 01;
+
+	for (x = 01; x < 100; x++)
+	{
+		printchar(x)
+			if (x < 99)
+			{
+				printchar(',');
+				printchar(' ');
+			}
+	}
+	printchar('\n');
+	return (0);
+}
