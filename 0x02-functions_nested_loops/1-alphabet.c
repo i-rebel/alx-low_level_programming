4cfecd954ad8cb2806052ca07778184c5318c982
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-26 15:32:41 -0700

    main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
+/**
+ * print_alphabet - prints alphabets in lowercase
+ *
+ * Return - Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
