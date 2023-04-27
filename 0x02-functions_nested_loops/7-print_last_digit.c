9cf2ac6e87fd9faacd08d27b255a2f5c8a743133
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-26 18:06:13 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: the last digit
+ * Return: Always 0
+ */
+
+int print_last_digit(int n)
+{
+	int y = n % 10;
+	return (y);
+}
