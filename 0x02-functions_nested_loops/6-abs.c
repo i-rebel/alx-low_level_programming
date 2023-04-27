59d5b380692317607c679794f4820f85a93673e1
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-26 17:40:41 -0700

    int abs_x = abs(x);

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: Always 0
+ */
+
+int _abs(int)
+{
+	int i;
+	int j = abs(i);
+
+	_putchar(j);
+	return (0);
+}
