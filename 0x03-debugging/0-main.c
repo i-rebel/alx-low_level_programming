e7de871924156041da1e7bb32b0076c12dcf7d09
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-27 14:31:32 -0700

    hhdh

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-* main  - tests function that prints positive or negative
-*
-* Return: Always 0 (Success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)
