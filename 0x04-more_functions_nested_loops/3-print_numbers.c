d2a420873b265464d104e3debda4fcb40d4a9ce6
Iyanuoluwa <otegataiwo@gmail.com>
2023-04-28 20:11:06 -0700

    hhh

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= 9; x++)
+	{_putchar(x);
+	}
+	_putchar('\n');
+}
